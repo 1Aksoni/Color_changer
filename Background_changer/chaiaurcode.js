330e4f64ff8ec4ef61ca7bafb3de04f07b24d9f9
1Aksoni <akshat2001soni@gmail.com>
2024-08-21 17:21:02 +0530

    click event added

--- Background_changer/chaiaurcode.js
+++ Background_changer/chaiaurcode.js
@@ -2,7 +2,7 @@ const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
 buttons.forEach(button => {
-    button.addEventListener('mouseover', e => {
+    button.addEventListener('click', e => {
         const color = e.target.id;
         body.style.backgroundColor = color;
     });
